a9c2707740181d326742b9023320d550bc90993e
Wiryono <wiryonolau@gmail.com>
2026-05-12 04:05:12 +0000

    Updates

--- assets/it-in-smb-C4dEhBuj.js
+++ assets/it-in-smb-C4dEhBuj.js
@@ -1,4 +1,4 @@
-import{t as e}from"./jsx-runtime-BOY_dY-B.js";import{t}from"./Carousel-CDcaLUwv.js";import{t as n}from"./index-Bl8j4Obe.js";var r=e(),i=[{title:`IT is seen as cost`,text:`
+import{t as e}from"./jsx-runtime-BOY_dY-B.js";import{t}from"./Carousel-bO6IHD1B.js";import{t as n}from"./index-CEJRg3_N.js";var r=e(),i=[{title:`IT is seen as cost`,text:`
         IT is commonly seen as a cost. 
         And honestly, that’s true.
         
